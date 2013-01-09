e58df4f2294993825744f230e5aee3d694aef318
AJ Christensen <aj@junglist.gen.nz>
2013-01-09 15:12:43 +1300

    [subrosa] [lein] just jar, not uber

--- recipes/default.rb
+++ recipes/default.rb
@@ -47,7 +47,7 @@ execute "extract subrosa" do
   creates File.join(path, "project.clj")
 end
 
-execute "lein deps && lein uberjar" do
+execute "lein deps && lein jar" do
   cwd path
   environment "LEIN_ROOT" => "true"
   user node['subrosa']['user']
